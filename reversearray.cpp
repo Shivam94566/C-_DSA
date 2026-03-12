87a1189525e83af98bc2b991ee936394b7c4547e
Shivam Bhardwaj <shivambhardwaj.94566@gmail.com>
2026-03-12 20:13:51 +0530

    Update reversearray.cpp

--- reversearray.cpp
+++ reversearray.cpp
@@ -6,7 +6,7 @@ function reverseArray(arr) {
     // in reversed order
     let temp = new Array(n);
   
-    // Copy elements from original array
+    // Copy elements from original array 
     // to temp in reverse order
     for (let i = 0; i < n; i++)
         temp[i] = arr[n - i - 1];
