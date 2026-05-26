ab7881a07b93ff594af98e74a80cf9bf60e1498e
Shivam Bhardwaj <shivambhardwaj.94566@gmail.com>
2026-05-26 21:28:16 +0530

    Refactor bubble sort implementation and main function

--- bubble.sort.cpp
+++ bubble.sort.cpp
@@ -1,34 +1,38 @@
- // C++ program for the implementation of Bubble sort
-#include <bits/stdc++.h> 
-using namespace std;
-//bubblesort
+#include <iostream>
+#include <vector>
+#include <algorithm> // for std::swap
 
-void bubbleSort(vector<int>& v) {  
-    int n = v.size();
-
-    // Outer loop that corresponds to the number of
-    // elements to be sorted
+void bubbleSort(std::vector<int>& arr) {
+    int n = arr.size();
+    bool swapped;
+    
+    // Outer loop for each pass
     for (int i = 0; i < n - 1; i++) {
-
-        // Last i elements are already
-        // in place  
+        swapped = false;
+        
+        // Inner loop to compare adjacent elements
+        // The last i elements are already in place
         for (int j = 0; j < n - i - 1; j++) {
-          
-          	// Comparing adjacent elements
-            if (v[j] > v[j + 1])
-              
-              	// Swapping if in the wrong order
-                swap(v[j], v[j + 1]);
+            if (arr[j] > arr[j + 1]) {
+                std::swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
         }
+        
+        // If no two elements were swapped by inner loop, then break
+        if (!swapped)
+            break;
     }
 }
 
 int main() {
-    vector<int> v = {5, 1, 4, 2, 8};
-
-    // Sorting the vector v
-    bubbleSort(v);
-    for (auto i : v)
-        cout << i << " ";
+    std::vector<int> data = {64, 34, 25, 12, 22, 11, 90};
+    
+    bubbleSort(data);
+    
+    std::cout << "Sorted array: ";
+    for (int num : data) {
+        std::cout << num << " ";
+    }
     return 0;
 }
